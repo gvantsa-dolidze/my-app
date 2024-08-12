42c54767d18a52db4e9984ca4b3d5829639accaa
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-08-13 01:01:22 +0400

    import Logo and Navbar components into Header and impoer Header component in App.js

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,13 @@
+import Logo from './Logo'
+import Navbar from './Navbar'
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="bg-slate-400">
+      <div className="w-[80%] m-auto p-5 flex justify-between">
+<Logo />
+<Navbar />
+      </div>
+    </div>
   )
 }
 
