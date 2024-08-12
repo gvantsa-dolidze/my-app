b535017740eb4b478fd39090374c8eb501ad1ff0
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-08-13 01:28:53 +0400

    import Sidebar and HomePage in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
-import Header from './components/Header'
-
+import "./App.css";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import HomePage from "./pages/HomePage";
 function App() {
   return (
     <div>
-        <Header />
+      <Header />
+      <div className="w-[80%] m-auto p-5 flex">
+        <Sidebar />
+        <HomePage />
+      </div>
     </div>
   );
 }
