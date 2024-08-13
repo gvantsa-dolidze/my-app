32b7fb346a923f3c3c92e3c5615bb04525b1ee77
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-08-13 23:51:18 +0400

    add title to Homepage component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,8 @@
 const HomePage = () => {
   return (
-    <div className="w-[100%] px-5 pt-5">HomePage</div>
+    <div className="w-[100%] px-5 pt-5">
+      <h2 className="text-center font-semibold text-2xl opacity-70">SpaceX Missions</h2>
+    </div>
   )
 }
 
