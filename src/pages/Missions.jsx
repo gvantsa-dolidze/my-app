c1693ecd8fb3dfa63e706d9a411d3c0a54b36bf1
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-08-13 23:12:03 +0400

    Fetch and display SpaceX missions data

--- src/pages/Missions.jsx
+++ src/pages/Missions.jsx
@@ -1,7 +1,34 @@
-const Missions = () => {
-  return (
-    <div className="w-[100%] px-5 pt-5">Missions</div>
-  )
-}
+import { useState, useEffect } from "react";
 
-export default Missions
+const Missions = () => {
+  const [missionsData, setMissionsData] = useState([]);
+  const getMissionsData = async () => {
+    try {
+      const res = await fetch("https://api.spacexdata.com/v3/missions");
+      if (!res.ok) {
+        throw new Error("response was not ok");
+      }
+      const data = await res.json();
+      // console.log(data);
+      setMissionsData(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    getMissionsData();
+  }, []);
+  return <div className="w-[100%] px-5 pt-5">
+    
+      {missionsData.map((missions, index)=>(
+<a href="/" key={index}>
+  <h2><span>Mission Name:</span>{missions.mission_name}</h2>
+  <h2><span>Mission Id:</span>{missions.mission_id}</h2>
+  <h2><span>Description:</span>{missions.description}</h2>
+</a>
+      ))}
+    
+  </div>;
+};
+
+export default Missions;
